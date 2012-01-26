38fc9f709a4b1162017edf0f02ea6945f1c8af48
Francesco Pierfederici <fpierfed@gmail.com>
2012-01-26 16:43:26 -0500

    initial

---
+++ example/sdpq/bin/workflow.py
@@ -0,0 +1,22 @@
+from eunomia import workflow as _workflow
+
+
+class ACSAsnWorkflow(_workflow.AcsSimpleWorkflow):
+    """
+    Workflow to process an ACS association.
+    """
+    pass
+
+
+
+class ACSExpWorkflow(_workflow.AcsSimpleWorkflow):
+    """
+    Workflow to process a single ACS exposure.
+    """
+    pass
+
+
+
+
+
+
