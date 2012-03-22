141832ad1eb12644bf6a788db267f839344b7b1c
Francesco Pierfederici <fpierfed@gmail.com>
2012-03-22 15:58:27 +0100

    now using PodBayDoors.py to fetch raw datasets.

--- example/sdpq2/src/plugins/acs.py
+++ example/sdpq2/src/plugins/acs.py
@@ -9,8 +9,40 @@ import os
 import subprocess
 
 from owl.utils import which
+from owl.config import DIRECTORIES_REPOSITORY
+
+# Needed for PodBayDoors.py
+ENV = {'OPUS_DB_SERVER': 'HARPOSQLA', 
+       'OPUS_DB': 'opus_rep', 
+       'DADS_DB_SERVER': 'HARPOSQLA', 
+       'DADS_DB': 'dadsops_rep',
+       'DELIVERY_HOST': 'dmsdevvm1.stsci.edu', 
+       'DELIVERY_DIR': '', 
+       'DELIVERY_USER': 'anonymous', 
+       'DELIVERY_PW': 'anonymous@stsci.edu', 
+       'ARCHIVE_USER': 'anonymous', 
+       'ARCHIVE_PASS': 'anonymous@stsci.edu', 
+       'SDPQ_DB_SERVER': 'dummy',
+       'SDPQ_DB': 'dummy'}
 
 
+
+
+def _setup_environment(datasetName):
+    """
+    Make sure that the environment variables that PodBayDoors.py needs are
+    defined. If not, get them from the constant ENV defined abuve. This does not
+    override existing definitions in os.environ, with the exception of 
+    $DELIVERY_DIR, which is always set to 
+        owl.config.DIRECTORIES_REPOSITORY/datasetName.
+    """
+    defined = os.environ.key()
+    for k, v in [(ky, vl) for (ky, vl) in ENV.items() \
+                 if ky not in defined and ky != 'DELIVERY_DIR']:
+        os.environ[k] = v
+    os.environ['DELIVERY_DIR'] = os.path.join(DIRECTORIES_REPOSITORY, 
+                                              datasetName)
+    return
 
 
 def process(entry):
@@ -25,30 +57,24 @@ def process(entry):
     
     Return None as workflow ID and the exit code of PodBayDoors.py
     """
+    _setup_environment(entry.datasetName)
+    
+    # POD/RAW files for dataset XXX are stored by PodBayDoors.py in the dir
+    # $DELIVERY_DIR, which we set to be owl.config.DIRECTORIES_REPOSITORY/XXX. 
+    # It is important to remember that $DELIVERY_DIR is the only environment 
+    # variable that we do override.
+    repo_dir = os.path.realpath(DIRECTORIES_REPOSITORY)
+    dst_dir = os.path.join(repo_dir, entry.datasetName)
+    if(os.path.isdir(repo_dir)):
+        os.makedirs(repo_dir)
+    
+    # Now is dst_dir exists, we have to choose what to do. We chose not to 
+    # download the files again and assume instead that they have been put there
+    # already.
+    if(os.path.exists(dst_dir)):
+        return((None, 0))
+    
     args = [which('PodBayDoors.py'), entry.datasetName]
     proc = subprocess.Popen(args, shell=False)
-    return(None, proc.wait())
-
-
-
-def process_list(entries):
-    """
-    process the list of sdpq2.SDPQueue entries `entries` and return the 
-    corresponding OWL workflow IDs as well as the workflow submission exit 
-    codes.
-    
-    At the moment, we just fetch the POD files associated to the given datasets 
-    from DADS, drop them all in the same directory and quit. We do this by 
-    calling 
-    
-        PodBayDoors.py <dataset name> <dataset name> <dataset name> ...
-    
-    Return None as workflow IDs and the exit code of PodBayDoors.py
-    """
-    args = [which('PodBayDoors.py'), ] + [e.datasetName for e in entries]
-    proc = subprocess.Popen(args, shell=False)
     err = proc.wait()
-    return([(None, err) for e in entries])
-
-
-
+    return((None, err))
