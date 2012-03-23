5caa177e7ff45d9a8b80dc712d26f7065cb0446f
Francesco Pierfederici <fpierfed@gmail.com>
2012-03-23 13:24:59 +0100

    MOved to a simpler mode of operation: dump all POD files in the same directory. Not complete yet.

--- example/sdpq2/src/plugins/acs.py
+++ example/sdpq2/src/plugins/acs.py
@@ -16,14 +16,6 @@ ENV = {'OPUS_DB_SERVER': 'HARPOSQLA',
        'OPUS_DB': 'opus_rep', 
        'DADS_DB_SERVER': 'HARPOSQLA', 
        'DADS_DB': 'dadsops_rep',
-       'DELIVERY_HOST': 'dmsdevvm1.stsci.edu', 
-       'DELIVERY_DIR': '', 
-       'DELIVERY_USER': 'anonymous', 
-       'DELIVERY_PW': 'anonymous@stsci.edu', 
-       'ARCHIVE_USER': 'anonymous', 
-       'ARCHIVE_PASS': 'anonymous@stsci.edu', 
-       'SDPQ_DB_SERVER': 'dummy',
-       'SDPQ_DB': 'dummy',
        'ACAREA': '/usr/local/sybase/stbin'}
 
 
@@ -33,17 +25,32 @@ def _setup_environment(datasetName):
     """
     Make sure that the environment variables that PodBayDoors.py needs are
     defined. If not, get them from the constant ENV defined abuve. This does not
-    override existing definitions in os.environ, with the exception of 
-    $DELIVERY_DIR, which is always set to 
-        owl.config.DIRECTORIES_REPOSITORY/datasetName.
+    override existing definitions in os.environ.
     """
     defined = os.environ.keys()
-    for k, v in [(ky, vl) for (ky, vl) in ENV.items() \
-                 if ky not in defined and ky != 'DELIVERY_DIR']:
+    for k, v in [(ky, vl) for (ky, vl) in ENV.items() if ky not in defined]:
         os.environ[k] = v
-    os.environ['DELIVERY_DIR'] = os.path.join(DIRECTORIES_REPOSITORY, 
-                                              datasetName)
     return
+
+
+def _get_pod_names(datasetName):
+    """
+    If `datasetName` is an association, determine all the corresponding 
+    exposures; otherwise set the exposure list to contain `datasetName` only.
+    Then, for each exposure in the exposure list, determine the POD/EDT dataset
+    names and return them all in a flat list.
+    
+    This is a wrapper around PodBayDoors.getExposures and PodBayDoors.findPod.
+    """
+    from PodBayDoors import getExposures, findPod
+    
+    exps = getExposures([datasetName, ])
+    allfiles = []
+    for key in exps.iterkeys():
+        results = findPod(exps[key])
+        allfiles.extend(results["podfiles"])
+        allfiles.extend(results["edtfiles"])
+    return(allfiles)
 
 
 def process(entry):
@@ -52,31 +59,24 @@ def process(entry):
     OWL workflow ID as well as the workflow submission exit code.
     
     At the moment, we just fetch the POD files associated to the given dataset 
-    from DADS, drop them in a directory and quit. We do this by calling 
+    from DADS, drop them all in the `DIRECTORIES_REPOSITORY` directory and quit.
+    We do this by calling routines in 
     
-        PodBayDoors.py <dataset name>
+        PodBayDoors.py
     
-    Return None as workflow ID and the exit code of PodBayDoors.py
+    Return None as workflow ID and the exit code of the file fetching process.
     """
+    err = 0
     _setup_environment(entry.datasetName)
     
-    # POD/RAW files for dataset XXX are stored by PodBayDoors.py in the dir
-    # $DELIVERY_DIR, which we set to be owl.config.DIRECTORIES_REPOSITORY/XXX. 
-    # It is important to remember that $DELIVERY_DIR is the only environment 
-    # variable that we do override.
+    # POD/EDT files are all stored in owl.config.DIRECTORIES_REPOSITORY
     repo_dir = os.path.realpath(DIRECTORIES_REPOSITORY)
-    dst_dir = os.path.join(repo_dir, entry.datasetName)
     if(not os.path.isdir(repo_dir)):
         os.makedirs(repo_dir)
     
-    # Now is dst_dir exists, we have to choose what to do. We chose not to 
-    # download the files again and assume instead that they have been put there
-    # already.
-    if(os.path.exists(dst_dir)):
-        return((None, 0))
-    os.makedirs(dst_dir)
+    # Retrieve the POD/EDT dataset names.
+    dataset_names = _get_pod_names(entry.datasetName)
     
-    args = [which('PodBayDoors.py'), entry.datasetName]
-    proc = subprocess.Popen(args, shell=False)
-    err = proc.wait()
+    # Print them out for now.
+    print(dataset_names)
     return((None, err))
