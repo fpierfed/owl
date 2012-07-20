06f7cd2eb8690044755ee2edf8ad1186e47374cf
Francesco Pierfederici <fpierfed@gmail.com>
2012-07-20 14:55:33 -0400

    Transitioning from job to classad

--- src/job.py
+++ src/job.py
@@ -1,20 +1,20 @@
 # Copyright (C) 2010 Association of Universities for Research in Astronomy(AURA)
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     1. Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-# 
+#
 #     2. Redistributions in binary form must reproduce the above
 #       copyright notice, this list of conditions and the following
 #       disclaimer in the documentation and/or other materials provided
 #       with the distribution.
-# 
+#
 #     3. The name of AURA and its representatives may not be used to
 #       endorse or promote products derived from this software without
 #       specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY AURA ``AS IS'' AND ANY EXPRESS OR IMPLIED
 # WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -57,9 +57,9 @@ def _parseClassAdValue(rawVal):
     except:
         pass
     raise(NotImplementedError('Unable to parse `%s`.' % (rawVal)))
-    
-    
-    
+
+
+
 def _parse(classAdText):
     """
     Given a multi-line ClassAd text, parse it and return the corresponding
@@ -68,30 +68,30 @@ def _parse(classAdText):
     """
     # First of all, handle line continuations.
     classAdText = MULTILINE_BUSTER.sub(' ', classAdText)
-    
+
     res = {}
     lines = classAdText.split('\n')
     for i in range(len(lines)):
         line = lines[i].strip()
-        
+
         # Handle empty lines.
         if(not(line)):
             continue
-        
-        # Handle the Queue command, which does not have an = sign. 
+
+        # Handle the Queue command, which does not have an = sign.
         if(line.lower().startswith('queue')):
             res['Instances'] = _extract_num_instances(line)
             continue
-        
+
         # Handle simple, full line comments.
         if(line.startswith('#')):
             continue
-        
+
         try:
             rawKey, rawVal = line.split('=', 1)
         except:
             raise(Exception('Cannot parse line "%s"' % (line)))
-        
+
         # Remember to strip any leading + sign from the key name.
         key = rawKey.strip()
         if(key[0] == '+'):
@@ -116,27 +116,31 @@ def _extract_num_instances(line):
     return(1)
 
 
-
-class Job(object):
+class ClassAd(object):
     @classmethod
     def newFromClassAd(cls, ad):
         """
         Given a Condor ClassAd text, parse it and create the corresponding Job
-        instance. Save the raw ClassAd as _rawClassAd instance variable for 
+        instance. Save the raw ClassAd as _rawClassAd instance variable for
         future reuse.
         """
         j = cls(**_parse(ad))
         j._rawClassAd = ad
         return(j)
-    
+
     def __init__(self, **kw):
         map(lambda (k, v): setattr(self, k, v), kw.items())
-        
+
         # Just to be consistent, if JobState is undefined or None, set it to
         # 'Starting'.
         if('JobState' not in kw.keys() or not kw['JobState']):
             self.JobState = unicode('Starting')
         return
+
+
+class Job(ClassAd):
+    pass
+
 
 
 
