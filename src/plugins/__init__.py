224d06d1e3dcc88d3ef516e168038b88558afd11
Francesco Pierfederici <fpierfed@gmail.com>
2013-01-15 16:27:57 -0500

    more user firendly messages when any given plugin cannot be imported.

--- src/plugins/__init__.py
+++ src/plugins/__init__.py
@@ -4,16 +4,20 @@ This is where support for different batch job execution system in implemented.
 try:
     import condor
 except:
+    print('Warning: condor plugin disabled.')
     pass
 try:
     import makefile
 except:
+    print('Warning: Makefile plugin disabled.')
     pass
 try:
     import xgrid
 except:
+    print('Warning: XGrid plugin disabled.')
     pass
 try:
     import spreader
 except:
+    print('Warning: Spreader plugin disabled.')
     pass
