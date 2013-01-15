47fe427abd6af12c46f8a04b9f013a097dfdf5d8
Francesco Pierfederici <fpierfed@gmail.com>
2013-01-15 16:28:22 -0500

    lots of work to make it functional.

--- src/plugins/spreader.py
+++ src/plugins/spreader.py
@@ -3,6 +3,8 @@ Define all Makefile-specific properties here.
 
 This is a bit of a hack... err... proof of concept, I mean ;-)
 """
+import collections
+import shutil
 import os
 
 from owl import dag
@@ -17,17 +19,26 @@ class DAG(dag.DAG):
     This is a spreader DAG. We assume that the comman-line argument do not have
     spaces!
     """
-    def execute(self):
+    def execute(self, work_dir):
         err = 0
-        running = _enqueue(self.roots)
+        running = _enqueue(self.roots, work_dir)
 
         # Now wait. A node is done if and only if al its instances are done.
         while(running):
             instances = running.pop(0)
-            done = [True for (p, n) in instances if p.is_ready()]
+            not_done = [(p, n) for (p, n) in instances if not p.is_ready()]
+            for (promise, node) in not_done:
+                promise.wait()
+            done = [(p, n) for (p, n) in instances if p.is_ready()]
+            for (promise, node) in done:
+                res = promise.result()
+                if(res['terminated'] or res['exit_code'] != 0):
+                    print(res)
+                    return(res['exit_code'])
+
             if(len(done) == len(instances)):
                 # Move to the next nodes.
-                running += _enqueue(instances[0][1].children)
+                running += _enqueue(instances[0][1].children, work_dir)
             else:
                 running.append(instances)
         return(err)
@@ -35,7 +46,16 @@ class DAG(dag.DAG):
 
 
 
-def _enqueue(nodes):
+def _copy_input_files(node, work_dir):
+    if(hasattr(node.job, 'transfer_input_files')):
+        paths = node.job.transfer_input_files.split(',')
+        for path in paths:
+            if(not os.path.exists(os.path.basename(path))):
+                shutil.copy(path, work_dir)
+    return
+
+
+def _enqueue(nodes, work_dir):
     """
     Submit the given nodes to the active spreader installation.
     """
@@ -43,21 +63,48 @@ def _enqueue(nodes):
     for node in nodes:
         instances = []
         for _id in range(node.job.Instances):
-            promise = client.async_call('system', _mkargv(node, _id))
-            instances.append(promise, node)
+            node = _expand_vars(node, _id)
+            _copy_input_files(node, work_dir)
+            promise = client.async_call('system',
+                                        _mkargv(node),
+                                        cwd=work_dir)
+            instances.append((promise, node))
         running.append(instances)
     return(running)
 
 
-def _mkargv(node, instance_id):
+def _expand_vars(node, instance_id):
+    mapping = {'$(Process)': str(instance_id)}
+
+    new_node = _copynode(node)
+    for key in new_node.job.__dict__.keys():
+        for var in mapping.keys():
+            value = getattr(new_node.job, key)
+            if(isinstance(value, basestring) and var in value):
+                setattr(new_node.job, key, value.replace(var, mapping[var]))
+            elif(isinstance(value, collections.Iterable)):
+                for i in range(len(value)):
+                    if(isinstance(value[i], basestring) and var in value):
+                        value[i] = value[i].replace(var, mapping[var])
+                setattr(new_node.job, key, value)
+    return(new_node)
+
+
+def _copynode(node):
+    new_node = dag.Node(name=node.name,
+                        script=node.script,
+                        children=node.children,
+                        parents=node.parents)
+    return(new_node)
+
+
+def _mkargv(node):
     """
     We assume that the comman-line argument do not have spaces!
     """
     # We have to do variable expansion by hand.
     # FIXME: Support spaces in job argument string.
-    argstr = node.job.Arguments.replace('$(Process)', str(instance_id))
-    exe = node.job.Executable.replace('$(Process)', str(instance_id))
-    return([exe, ] + argstr.split())
+    return([node.job.Executable, ] + node.job.Arguments.split())
 
 
 def submit(dagName, workDir):
@@ -69,15 +116,13 @@ def submit(dagName, workDir):
     # All the files we need (the .dag file and the .job files) are in `workDir`
     # and have the names defined in the .dag file (which we are given as
     # `dagName`). So, first thing is to parse `dagName`.
-    dag = DAG.new_from_classad(open(os.path.join(workDir, dagName)).read(),
-                               workDir)
+    print(workDir)
 
-    # Create the makefile
-    f = open(os.path.join(workDir, 'Makefile'), 'w')
-    f.write(dag.to_makefile())
-    f.close()
-
-    print('Makefile written in work directory %s' % (workDir))
-    return(0)
+    here = os.getcwd()
+    os.chdir(workDir)
+    dag = DAG.new_from_dag(open(os.path.join(workDir, dagName)).read(), workDir)
+    err = dag.execute(workDir)
+    os.chdir(here)
+    return(err)
 
 
